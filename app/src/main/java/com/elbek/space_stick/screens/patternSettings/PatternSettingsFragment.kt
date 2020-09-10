0d100db88428bac3f390c9383d40cf09c2629aa0
ElbekM <elbek.987598@gmail.com>
2020-09-11 03:58:00 +0700

    created pattern settings design, update text size, fonts, colors

--- app/src/main/java/com/elbek/space_stick/screens/patternSettings/PatternSettingsFragment.kt
+++ app/src/main/java/com/elbek/space_stick/screens/patternSettings/PatternSettingsFragment.kt
@@ -1,11 +1,15 @@
 package com.elbek.space_stick.screens.patternSettings
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.elbek.space_stick.R
 import com.elbek.space_stick.common.mvvm.BaseDialogFragment
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import kotlinx.android.synthetic.main.fragment_pattern_settings.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PatternSettingsFragment : BaseDialogFragment<PatternSettingsViewModel>() {
@@ -28,9 +32,27 @@ class PatternSettingsFragment : BaseDialogFragment<PatternSettingsViewModel>() {
     override fun bindViewModel() {
         super.bindViewModel()
     }
-
+    //TODO: refactoring
     private fun initViews() {
+        val sin = LineGraphSeries<DataPoint>()
+        val cos = LineGraphSeries<DataPoint>()
 
+        var x: Double = 0.0
+        var y: Double
+
+        val numDataPoints = 500
+        for (num in 0..numDataPoints) {
+            x += 0.1
+            y = kotlin.math.sin(x)
+            val y2 = kotlin.math.cos(x)
+            sin.appendData(DataPoint(x, y), true, 60)
+            cos.appendData(DataPoint(x, y2), true, 60)
+        }
+
+        sin.color = Color.RED
+        cos.color = Color.BLUE
+        graphView.addSeries(sin)
+        graphView.addSeries(cos)
     }
 
     companion object {
