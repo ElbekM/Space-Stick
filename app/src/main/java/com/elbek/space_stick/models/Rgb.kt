29d93c9d18689599fbd20b077f4de1adcaf50a82
ElbekM <elbek.987598@gmail.com>
2020-10-08 20:37:14 +0700

    Move data classes into models package

---
+++ app/src/main/java/com/elbek/space_stick/models/Rgb.kt
@@ -0,0 +1,7 @@
+package com.elbek.space_stick.models
+
+data class Rgb(
+    val r: Int,
+    val g: Int,
+    val b: Int
+)
--- app/src/main/java/com/elbek/space_stick/screens/patternSettings/RgbSettingsViewModel.kt
+++ app/src/main/java/com/elbek/space_stick/screens/patternSettings/RgbSettingsViewModel.kt
@@ -3,6 +3,8 @@ package com.elbek.space_stick.screens.patternSettings
 import android.app.Application
 import com.elbek.space_stick.api.StickService
 import com.elbek.space_stick.common.mvvm.BaseViewModel
+import com.elbek.space_stick.models.ColorsType
+import com.elbek.space_stick.models.Rgb
 import kotlinx.coroutines.launch
 
 class RgbSettingsViewModel(private val apiService: StickService, application: Application) :
@@ -31,24 +33,5 @@ class RgbSettingsViewModel(private val apiService: StickService, application: Ap
                 }
             }
         }
-    }
-
-    data class Rgb(
-        val r: Int,
-        val g: Int,
-        val b: Int
-    )
-
-    enum class ColorsType(val color: Rgb) {
-        WHITE(Rgb(255, 255, 255)),
-        RED(Rgb(255, 0, 0)),
-        VIOLET(Rgb(181, 0, 175)),
-        NEON(Rgb(123, 0, 183)),
-        DARK_BLUE(Rgb(0, 0, 255)),
-        BLUE(Rgb(0, 164, 246)),
-        GREEN(Rgb(0, 255, 0)),
-        YELLOW(Rgb(251, 237, 55)),
-        ORANGE(Rgb(255, 142, 0)),
-        GRAY(Rgb(81, 124, 136))
     }
 }
