8e3603a51c3d56da456eb0c549a2b6f3d922ef30
ElbekM <elbek.987598@gmail.com>
2020-10-16 23:49:00 +0700

    Added color picker dialog

--- app/src/main/java/com/elbek/space_stick/screens/patternSettings/RgbSettingsFragment.kt
+++ app/src/main/java/com/elbek/space_stick/screens/patternSettings/RgbSettingsFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.elbek.space_stick.R
 import com.elbek.space_stick.common.mvvm.BaseDialogFragment
 import com.elbek.space_stick.models.ColorType
 import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
 import kotlinx.android.synthetic.main.fragment_rgb_settings.*
 import kotlinx.android.synthetic.main.view_color_list.*
+import kotlinx.android.synthetic.main.view_custom_colors.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RgbSettingsFragment : BaseDialogFragment<RgbSettingsViewModel>() {
@@ -29,9 +31,21 @@ class RgbSettingsFragment : BaseDialogFragment<RgbSettingsViewModel>() {
         bindViewModel()
     }
 
+    override fun bindViewModel() {
+        super.bindViewModel()
+
+        viewModel.customColorsLayoutVisible.observe {
+            customColorsLayout.isVisible = it ?: false
+        }
+
+        viewModel.showColorPickerDialogLiveEvent.observe {
+            viewModel.showColorPickerDialog(requireContext())
+        }
+    }
+
     private fun initViews() {
         backImageView.setOnClickListener { close() }
-
+        addNewColorButton.setOnClickListener { viewModel.onAddNewColorButtonClicked() }
         colorPickerView.setColorListener(ColorEnvelopeListener { envelope, _ ->
             viewModel.onColorPickerSelected(envelope.argb)
         })
--- app/src/main/java/com/elbek/space_stick/screens/patternSettings/RgbSettingsViewModel.kt
+++ app/src/main/java/com/elbek/space_stick/screens/patternSettings/RgbSettingsViewModel.kt
@@ -1,14 +1,31 @@
 package com.elbek.space_stick.screens.patternSettings
 
 import android.app.Application
+import android.content.Context
 import com.elbek.space_stick.api.StickService
 import com.elbek.space_stick.common.mvvm.BaseViewModel
+import com.elbek.space_stick.common.mvvm.commands.LiveEvent
+import com.elbek.space_stick.common.mvvm.commands.SingleLiveEvent
 import com.elbek.space_stick.models.ColorType
 import com.elbek.space_stick.models.Rgb
+import com.skydoves.colorpickerview.ColorPickerDialog
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
 import kotlinx.coroutines.launch
 
 class RgbSettingsViewModel(private val apiService: StickService, application: Application) :
     BaseViewModel(application) {
+
+    val showColorPickerDialogLiveEvent = LiveEvent()
+    val customColorsLayoutVisible = SingleLiveEvent<Boolean>()
+
+    override fun back() {
+        customColorsLayoutVisible.value.let { visible ->
+            if (visible != null && visible)
+                customColorsLayoutVisible.value = false
+            else
+                super.back()
+        }
+    }
 
     fun onColorPickerSelected(colorArray: IntArray) {
         val color = Rgb(colorArray[1], colorArray[2], colorArray[3])
@@ -20,7 +37,27 @@ class RgbSettingsViewModel(private val apiService: StickService, application: Ap
     }
 
     fun onCustomColorsClicked() {
-        //TODO: Open dialog with custom colors
+        customColorsLayoutVisible.value = true
+    }
+
+    fun onAddNewColorButtonClicked() {
+        showColorPickerDialogLiveEvent.call()
+    }
+
+    fun showColorPickerDialog(context: Context) {
+        ColorPickerDialog.Builder(context)
+            .setTitle("ColorPicker Dialog")
+            .setPositiveButton("Save", ColorEnvelopeListener { envelope, _ ->
+                //TODO: Save color into database
+                envelope.argb
+            })
+            .setNegativeButton("Cancel") { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }
+            .attachAlphaSlideBar(false)
+            .attachBrightnessSlideBar(false)
+            .setBottomSpace(12)
+            .show()
     }
 
     private fun setColor(color: Rgb) {
