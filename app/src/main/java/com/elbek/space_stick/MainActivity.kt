8250d4f17529949a122341ea26d574f27e2429ce
ElbekM <elbek.987598@gmail.com>
2020-10-24 15:41:59 +0700

    fx

--- app/src/main/java/com/elbek/space_stick/MainActivity.kt
+++ app/src/main/java/com/elbek/space_stick/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
+import com.elbek.space_stick.screens.main.MainFragment
 import com.elbek.space_stick.screens.stick.StickFragment
 
 class MainActivity : AppCompatActivity() {
@@ -23,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun launchMainScreen() {
-        StickFragment.newInstance("s").apply {
+        MainFragment.newInstance().apply {
             supportFragmentManager.beginTransaction()
                 .add(this, this::class.java.name)
                 .addToBackStack(this::class.java.name)
